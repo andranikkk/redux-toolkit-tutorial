37529b1ee003b23fc75da0145609c1f8bbb9a283
andranikkk <andranikayvazyan46@gmail.com>
2025-12-15 13:42:31 +0400

    initial commit

--- final/src/features/cart/cartSlice.js
+++ final/src/features/cart/cartSlice.js
@@ -1,82 +1,110 @@
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-import axios from 'axios';
-import { openModal } from '../modal/modalSlice';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import axios from 'axios'
 
-const url = 'https://course-api.com/react-useReducer-cart-project';
+const url = 'https://course-api.com/react-useReducer-cart-project'
 
 const initialState = {
-  cartItems: [],
-  amount: 4,
-  total: 0,
-  isLoading: true,
-};
+	cartItems: [
+		{
+			id: 'rec1JZlfCIBOPdcT2',
+			title: 'Samsung Galaxy S8',
+			price: '399.99',
+			img: 'https://images2.imgbox.com/c2/14/zedmXgs6_o.png',
+			amount: 1,
+		},
+		{
+			id: 'recB6qcHPxb62YJ75',
+			title: 'google pixel',
+			price: '499.99',
+			img: 'https://images2.imgbox.com/fb/3d/O4TPmhlt_o.png',
+			amount: 1,
+		},
+		{
+			id: 'recdRxBsE14Rr2VuJ',
+			title: 'Xiaomi Redmi Note 2',
+			price: '699.99',
+			img: 'https://images2.imgbox.com/4f/3d/WN3GvciF_o.png',
+			amount: 1,
+		},
+		{
+			id: 'recwTo160XST3PIoW',
+			title: 'Samsung Galaxy S7',
+			price: '599.99 ',
+			img: 'https://images2.imgbox.com/2e/7c/yFsJ4Zkb_o.png',
+			amount: 1,
+		},
+	],
+	amount: 4,
+	total: 0,
+	isLoading: true,
+}
 
 export const getCartItems = createAsyncThunk(
-  'cart/getCartItems',
-  async (name, thunkAPI) => {
-    try {
-      // console.log(name);
-      // console.log(thunkAPI);
-      // console.log(thunkAPI.getState());
-      // thunkAPI.dispatch(openModal());
-      const resp = await axios(url);
+	'cart/getCartItems',
+	async (name, thunkAPI) => {
+		try {
+			// console.log(name);
+			// console.log(thunkAPI);
+			// console.log(thunkAPI.getState());
+			// thunkAPI.dispatch(openModal());
+			const resp = await axios(url)
 
-      return resp.data;
-    } catch (error) {
-      return thunkAPI.rejectWithValue('something went wrong');
-    }
-  }
-);
+			return resp.data
+		} catch (error) {
+			return thunkAPI.rejectWithValue('something went wrong')
+		}
+	}
+)
 
 const cartSlice = createSlice({
-  name: 'cart',
-  initialState,
-  reducers: {
-    clearCart: (state) => {
-      state.cartItems = [];
-    },
-    removeItem: (state, action) => {
-      const itemId = action.payload;
-      state.cartItems = state.cartItems.filter((item) => item.id !== itemId);
-    },
-    increase: (state, { payload }) => {
-      const cartItem = state.cartItems.find((item) => item.id === payload.id);
-      cartItem.amount = cartItem.amount + 1;
-    },
-    decrease: (state, { payload }) => {
-      const cartItem = state.cartItems.find((item) => item.id === payload.id);
-      cartItem.amount = cartItem.amount - 1;
-    },
-    calculateTotals: (state) => {
-      let amount = 0;
-      let total = 0;
-      state.cartItems.forEach((item) => {
-        amount += item.amount;
-        total += item.amount * item.price;
-      });
-      state.amount = amount;
-      state.total = total;
-    },
-  },
-  extraReducers: (builder) => {
-    builder
-      .addCase(getCartItems.pending, (state) => {
-        state.isLoading = true;
-      })
-      .addCase(getCartItems.fulfilled, (state, action) => {
-        // console.log(action);
-        state.isLoading = false;
-        state.cartItems = action.payload;
-      })
-      .addCase(getCartItems.rejected, (state, action) => {
-        console.log(action);
-        state.isLoading = false;
-      });
-  },
-});
+	name: 'cart',
+	initialState,
+	reducers: {
+		clearCart: state => {
+			state.cartItems = []
+		},
+		removeItem: (state, action) => {
+			const itemId = action.payload
+			state.cartItems = state.cartItems.filter(item => item.id !== itemId)
+		},
+		increase: (state, { payload }) => {
+			const cartItem = state.cartItems.find(item => item.id === payload.id)
+			cartItem.amount = cartItem.amount + 1
+		},
+		decrease: (state, { payload }) => {
+			const cartItem = state.cartItems.find(item => item.id === payload.id)
+			cartItem.amount = cartItem.amount - 1
+		},
+		calculateTotals: state => {
+			let amount = 0
+			let total = 0
+			state.cartItems.forEach(item => {
+				amount += item.amount
+				total += item.amount * item.price
+			})
+			state.amount = amount
+			state.total = total
+		},
+	},
+	extraReducers: builder => {
+		builder
+			.addCase(getCartItems.pending, state => {
+				state.isLoading = true
+			})
+			.addCase(getCartItems.fulfilled, (state, action) => {
+				// console.log(action);
+				state.isLoading = false
+				state.cartItems = action.payload
+			})
+			.addCase(getCartItems.rejected, (state, action) => {
+				console.log(action)
+				state.isLoading = false
+			})
+	},
+})
 
 // console.log(cartSlice);
 export const { clearCart, removeItem, increase, decrease, calculateTotals } =
-  cartSlice.actions;
+	cartSlice.actions
 
-export default cartSlice.reducer;
+export default cartSlice.reducer
